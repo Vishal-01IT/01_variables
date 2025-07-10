28f12677b3b6a278cccccba6005a0435db13bf97
Vishal-01IT <vishalsaini.ngd@gmail.com>
2025-07-10 22:40:17 +0530

    dataTypes

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,7 +1,23 @@
-"use strict"; // treat all js code as newer version 
+"use strict"; /* treat all JS code as newer version */
 
-// alert(3 + 3) we are using nodejs, not browser
+// alert( 3 + 3 ) We are using Node.js, not browser
 
-console.log(3+3)
+console.log( 3 + 3 ); console.log("Vishal")  /* Code readability that should be high */
 
-console.log(vishal)
+let name = "Vishal"
+let age = 18
+let isloggedIn = false
+
+// string data = alphabatic words
+// stting = 18 numer dighet
+// number = 2 to Power 53 
+// boolean = true,false 
+// null = stand lone value (isme ager koi value na do to null likh skahte hai ex- let age = null) Object hai
+// undefined = (Abhi value descide nahi huai hai) Value bhi hai jo print hoti hai
+// bigint = big number data like (facebook, trading)
+// symbol = unique ke liye
+// Object = 
+
+
+console.log(typeof undefined); //undefind hi ata hai
+console.log(typeof null); //object
