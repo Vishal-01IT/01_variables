277edde5326e101bab7ab85dbfc1b2bca5d6d48d
Vishal-01IT <vishalsaini.ngd@gmail.com>
2025-07-18 21:51:05 +0530

    datatypes

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,42 @@
+// Primitive 
+
+// 7 Types : String(), Number(), Boolearn(Yes/No), null(null,Ematy,khali), undefined, Symbol(Kise bhi value ko uqnie banana), BigInt(Badi Value ke liye) 
+
+
+const score = 100
+const scoreValue = 100.3
+
+const isloggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId)
+
+const bigNumber = 34444444444444444445n
+
+
+// Reference (Non Primitive)
+
+// Array, Objects, Functions
+
+
+const heros = ["Shaktiman", "naagraj", "doga"]; //array []
+
+// Object
+let myObj = {               
+   name: "Vishal", 
+   age: 22,
+}   
+
+
+// function 
+
+const mFunction = function(){
+    console.log("Hello world");    
+}
+
+console.log(typeof bigNumber);
+
